532162b530fb4896328ad1c99a1e8d2fc6e63f02
Istiak <mhistiak2@gmail.com>
2024-10-18 17:04:15 +0600

    nextjs routing

---
+++ components/Navbar.jsx
@@ -0,0 +1,29 @@
+import Link from "next/link";
+
+const Navbar = () => {
+  return (
+    <div className="w-full bg-purple-600 py-5 px-3">
+      <div className="max-w-7xl mx-auto flex justify-between">
+        <div className="bg-white rounded py-2 px-4 ">
+          <h2 className="text-xl text-purple-600 font-bold">Logo</h2>
+        </div>
+
+        <ul className="flex gap-x-4 items-center text-white">
+          <li>
+            <Link href={"/"}>Home</Link>
+          </li>
+          <li>
+            <Link href={"/about"}>About</Link>
+          </li>
+          <li>
+            <Link href={"/portfolio"}>Portfolio</Link>
+          </li>
+          <li>
+            <Link href={"/contact"}>Contact</Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+export default Navbar;
