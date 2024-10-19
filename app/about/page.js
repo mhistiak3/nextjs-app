782f27bcf4224f48035cff27f82fdaf8708f501a
Istiak <mhistiak2@gmail.com>
2024-10-19 17:06:23 +0600

    image optimize

--- app/about/page.js
+++ app/about/page.js
@@ -1,9 +1,13 @@
 import Button from "@/components/Button";
-
+import Image from "next/image";
+import logo from "@/public/logo.png";
 const About = () => {
   return (
     <div className="flex items-center justify-center h-screen bg-gray-100">
       <div className="p-8 bg-white rounded-lg shadow-md">
+        <div className="w-[100px]">
+          <Image src={logo} alt="Logo" quality={100} placeholder="blur" />
+        </div>
         <h1 className="text-2xl font-bold mb-4">About</h1>
         <p className="text-gray-700">
           Welcome to the About page. Here we share our story and mission.
