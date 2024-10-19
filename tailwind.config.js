575421684230be856ebf35d9e188f027b419495f
Istiak <mhistiak2@gmail.com>
2024-10-19 17:19:39 +0600

    font  optimize

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,9 @@ module.exports = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      fontFamily: {
+        mono: ["var(--font-roboto-mono)"],
+      },
     },
   },
   plugins: [],
